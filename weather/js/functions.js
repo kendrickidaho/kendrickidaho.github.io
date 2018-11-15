fcf240f7902d5be07fa54c1ded738b8e02bc1f68
kendrickidaho <vil17009@byui.edu>
2018-11-14 18:45:08 -0700

    Javascript

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,2 +1,31 @@
 // Weather site JavaScript functions 
 console.log('My javascript is being read.');
+
+
+
+
+
+
+ // Calculate the Windchill
+ function buildWC(speed, temp) {
+    const feelTemp = document.getElementById('feels');
+   
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+   
+    // Round the answer down to integer
+    wc = Math.floor(wc);
+   
+    // If chill is greater than temp, return the temp
+    wc = (wc > temp)?temp:wc;
+   
+    // Display the windchill
+    console.log(wc);
+    // wc = 'Feels like '+wc+'°F';
+    feelTemp.innerHTML = wc;
+    }
+
+
+
+
