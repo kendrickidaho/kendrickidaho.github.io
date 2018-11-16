52090be3d63ef361d009d97cdfd07a2e843a45d8
kendrickidaho <vil17009@byui.edu>
2018-11-16 15:39:35 -0700

    save

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -28,6 +28,53 @@ buildWC(speed, temp);
     // wc = 'Feels like '+wc+'°F';
     feelTemp.innerHTML = wc;
     }
+    <div id="dial" class="w"></div>
+
+    // Wind Dial Function
+function windDial(direction){
+   // Get the container
+   const dial = document.getElementById("dial");
+   console.log(direction);
+   // Determine the dial class
+   switch (direction){
+    case "North":
+    case "N":
+     dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+     break;
+    case "NE":
+    case "NNE":
+    case "ENE":
+     dial.setAttribute("class", "ne");
+     break;
+    case "NW":
+    case "NNW":
+    case "WNW":
+     dial.setAttribute("class", "nw");
+     break;
+    case "South":
+    case "S":
+     dial.setAttribute("class", "s");
+     break;
+    case "SE":
+    case "SSE":
+    case "ESE":
+     dial.setAttribute("class", "se");
+     break;
+    case "SW":
+    case "SSW":
+    case "WSW":
+     dial.setAttribute("class", "sw");
+     break;
+    case "East":
+    case "E":
+     dial.setAttribute("class", "e");
+     break;
+    case "West":
+    case "W":
+     dial.setAttribute("class", "w");
+     break;
+   }
+  }
 
 
 
