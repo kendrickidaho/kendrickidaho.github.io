5a792f380e18754403ce02a6f6b8eb5d702269d2
kendrickidaho <vil17009@byui.edu>
2018-11-24 23:32:07 -0700

    new info weather

---
+++ Sandbox/loctest.js
@@ -0,0 +1,32 @@
+// these functions will work together to get weather informaton for the current location and populate a web page with the data
+'use strict';
+
+
+
+
+
+// Gets longitude and latitude of current location
+function getGeoLocation() {
+
+} // end getGeoLocation
+const STATUS = document.getElementById('status');
+ STATUS.innerHTML = 'Getting Location...';
+ // Call the function to get our location
+getGeoLocation();
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+     const LAT = position.coords.latitude;
+     const LONG = position.coords.longitude;
+  
+     // Combine the values
+     const LOCALE = LAT + "," + LONG;
+     console.log(`Lat and Long are: ${LOCALE}.`);
+     // Call getCode function, send locale
+     getCode(LOCALE);
+  
+  
+    })
+   } else {
+    STATUS.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+   } // end else
+  } //end getGeoLocation
